d10e67bfcbe8f0aa65f3a80e5b4cb651d9134353
DevPelumi <60540561+DevPelumi@users.noreply.github.com>
2022-06-06 09:36:59 -0230

    refactored & added axios

--- src/App.js
+++ src/App.js
@@ -3,24 +3,36 @@ import Footer from "./components/layout/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
-import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
+import { GithubProvider } from './context/github/GithubContext'
+import { AlertProvider } from "./context/Alert/AlertContext";
+import Alert from "./components/layout/Alert";
 
 function App() {
   return (
-    <BrowserRouter>
-    <div className="flex flex-col justify-between h-screen">
-      <Navbar title="Github Finder"/>
-      <main className="container mx-auto px-3 pb-12">
-      <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/notfound" element={<NotFound/>}/>
-      <Route path="/*" element={<NotFound/>}/>
-     </Routes>
-      </main>
-     <Footer footerYear={2022}/>
-    </div>
-    </BrowserRouter>
+    <GithubProvider>
+    <AlertProvider>
+     <Router>
+     <div className="flex flex-col justify-between h-screen">
+       <Navbar title="Github Finder"/>
+       <main className="container mx-auto px-3 pb-12">
+       <Routes>
+       <Route path="/" element={
+         <>
+          <Alert/>
+          <Home/>
+         </> 
+       }/>
+       <Route path="/about" element={<About/>}/>
+       <Route path="/notfound" element={<NotFound/>}/>
+       <Route path="/*" element={<NotFound/>}/>
+       </Routes>
+       </main>
+      <Footer footerYear={2022}/>
+     </div>
+     </Router>
+    </AlertProvider>
+    </GithubProvider>
   );
 }
 
---
+++ src/context/github/githubReducer.js
@@ -0,0 +1,22 @@
+
+const  githubReducer = (state, action) =>{
+    switch(action.type){
+        case 'GET_USER': return{
+            ...state, 
+            users: action.payload,
+            loading: false
+        }
+        case 'CLEAR_USERS': return{
+            ...state,
+            users: [],
+        }
+        case 'SET_LOADING':
+            return {
+                ...state, loading: true
+            }
+        default:
+            return state
+    }
+}
+
+export default githubReducer
